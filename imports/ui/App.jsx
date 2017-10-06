f1ac8441dfb82c45b40479eae18add44d54b3de5
wilkin <wilkin.ng@gmail.com>
2017-10-06 15:28:16 -0700

    have feature to hide completed

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -7,9 +7,20 @@ import { Tasks } from '../api/tasks.js'; // the data model
 
 // App component - represents the whole app
 class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            hideCompleted: false,
+        };
+    }
 
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        let filteredTasks = this.props.tasks;
+        if (this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -27,13 +38,27 @@ class App extends Component {
         // Clear form
         ReactDOM.findDOMNode(this.refs.textInput).value = '';
     }
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
 
     render() {
         return (
             <div className="container">
               <header>
-                <h1>Todo List</h1>
+                <h1>Todo List  ({this.props.incompleteCount})</h1>
               </header>
+                <label className="hide-completed">
+                    <input
+                        type="checkbox"
+                        readOnly
+                        checked={this.state.hideCompleted}
+                        onClick={this.toggleHideCompleted.bind(this)}
+                    />
+                    Hide Completed Tasks
+                </label>
                 <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
                     <input
                         type="text"
@@ -52,10 +77,13 @@ class App extends Component {
 
 App.propTypes = {
     tasks: PropTypes.array.isRequired,
+    incompleteCount: PropTypes.number.isRequired,
 };
 
+// what to pass as prop
 export default createContainer(() => {
     return {
         tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
+        incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
     };
 }, App);
